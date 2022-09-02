ec45f9fecf4fd1f338b2eeafaab3668b8f9e55e3
Nut Pinyo <bomb.np@gmail.com>
2022-09-02 14:58:36 +0700

    feat: add flow

---
+++ edge/__init__.py
@@ -0,0 +1,8 @@
+print("Starting Smart Room edge processor...")
+while True:
+    # 1. sample image from video source
+    # 2. send image to neural network, receive classification results
+    # 3. draw output results on image
+    # 4. send results back to backend server
+    # 5. send control signals to lights
+    pass
